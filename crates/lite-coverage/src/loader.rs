59263524f32494cebb4558e0771ecf3bdc6a50a2
Boris Astardzhiev <boris.astardzhiev@gmail.com>
2025-07-14 16:39:10 +0300

    pinocchio: sol_get_last_restart_slot

--- crates/lite-coverage/src/loader.rs
+++ crates/lite-coverage/src/loader.rs
@@ -296,6 +296,14 @@ pub extern "C" fn sol_get_epoch_rewards_sysvar(addr: *mut u8) -> u64 {
 }
 
 #[no_mangle]
+pub extern "C" fn sol_get_last_restart_slot(addr: *mut u8) -> u64 {
+    crate::stubs::SYSCALL_STUBS
+        .read()
+        .unwrap()
+        .sol_get_last_restart_slot(addr)
+}
+
+#[no_mangle]
 pub extern "C" fn sol_log_pubkey(pubkey: *const u8) {
     let pubkey = unsafe {
         let mut inner = [0u8; 32];
