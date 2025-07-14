eb0982c316f8257343317f2e02ecc137fc7f12cf
Boris Astardzhiev <boris.astardzhiev@gmail.com>
2025-07-14 09:57:51 +0300

    Initial commit adapting pinocchio programs. Moreover the entrypoint logic is unified. Still messy - let's clean up next

--- crates/lite-coverage/src/loader.rs
+++ crates/lite-coverage/src/loader.rs
@@ -2,6 +2,7 @@ use crate::{
     stubs::{StubsManager, SyscallStubsApi, UnimplementedSyscallStubs, WrapperSyscallStubs},
     types::LiteCoverageError,
 };
+use core::str;
 use libloading::{Library, Symbol};
 use solana_program::{
     account_info::AccountInfo, entrypoint::ProgramResult, program_error::ProgramError,
@@ -12,14 +13,8 @@ use std::{
     sync::{atomic::AtomicPtr, Mutex},
 };
 
-// type ProgramCEntrypoint = unsafe extern "C" fn(input: *mut u8) -> u64;
-type ProgramRustEntryPoint = unsafe extern "C" fn() -> *const ();
+type ProgramEntrypoint = unsafe extern "C" fn(input: *mut u8) -> u64;
 type ProgramSetSyscallStubsApi = unsafe extern "C" fn(stubs_api: SyscallStubsApi);
-type Entrypoint = for<'a, 'b, 'info, 'c> fn(
-    &'a Pubkey,
-    &'b [AccountInfo<'info>],
-    &'c [u8],
-) -> Result<(), ProgramError>;
 
 lazy_static::lazy_static! (
     pub static ref PROGRAMS_MAP: Mutex<HashMap<Pubkey, AtomicPtr<()>>> = Mutex::new(HashMap::new());
@@ -28,7 +23,7 @@ lazy_static::lazy_static! (
 pub fn entrypoint<'info>(
     program_id: &Pubkey,
     accounts: &[AccountInfo<'info>],
-    data: &[u8],
+    _data: &[u8],
 ) -> ProgramResult {
     let map = PROGRAMS_MAP.lock().unwrap();
     let entry = map
@@ -36,12 +31,123 @@ pub fn entrypoint<'info>(
         .unwrap()
         .load(std::sync::atomic::Ordering::Relaxed);
     let fn_ptr = entry as *const ();
-    let entry: for<'a, 'b, 'info2, 'c> fn(
-        &'a Pubkey,
-        &'b [AccountInfo<'info2>],
-        &'c [u8],
-    ) -> ProgramResult = unsafe { std::mem::transmute(fn_ptr) };
-    entry(program_id, accounts, data)
+    let entry: ProgramEntrypoint = unsafe { std::mem::transmute(fn_ptr) };
+
+    // Serialize entrypoint parameters with SBF ABI
+    let invoke_context: &solana_program_test::InvokeContext<'_> =
+        solana_program_test::get_invoke_context() as &_;
+    let transaction_context = &invoke_context.transaction_context;
+    let instruction_context = transaction_context
+        .get_current_instruction_context()
+        .map_err(|_| ProgramError::InvalidArgument)?;
+
+    let (mut parameter_bytes, _, _) =
+        solana_bpf_loader_program::serialization::serialize_parameters(
+            transaction_context,
+            instruction_context,
+            true, // copy_account_data // There is no VM so direct mapping can not be implemented here
+        )
+        .map_err(|_| ProgramError::InvalidArgument)?;
+
+    let ix_input_before = parameter_bytes.as_slice().to_vec();
+    let res = unsafe { entry(parameter_bytes.as_slice().as_ptr() as *mut _) };
+    if ix_input_before != parameter_bytes.as_slice() {
+        // Deserialize data back into instruction params
+        let (_, updated_account_infos, _) = unsafe {
+            solana_program_entrypoint::deserialize(
+                &mut parameter_bytes.as_slice_mut()[0] as *mut u8,
+            )
+        };
+
+        // println!(
+        //     "DIFFERENT inputs, is data different: {}",
+        //     data != updated_data
+        // );
+        // Persist the changes
+        println!(
+            "accounts.len(): {}, updated_account_infos.len(): {}",
+            accounts.len(),
+            updated_account_infos.len()
+        );
+        let _ = std::fs::write(
+            "/tmp/new_accounts.txt",
+            format!("{:#?}", updated_account_infos),
+        );
+        // #[allow(clippy::transmute_ptr_to_ptr)]
+        // #[allow(mutable_transmutes)]
+        // let accounts = unsafe { std::mem::transmute::<&[AccountInfo<'info>], &mut [AccountInfo<'info>]>(accounts) };
+        let len = accounts.len();
+        for i in 0..len {
+            (**accounts[i].lamports.borrow_mut()) = updated_account_infos[i].lamports();
+            let new_data = updated_account_infos[i].data.borrow_mut().to_vec();
+            let boxed: Box<[u8]> = new_data.into_boxed_slice();
+            let leaked = Box::leak(Box::new(boxed));
+            *accounts[i].data.borrow_mut() = leaked;
+
+            // unsafe {
+            //     (*accounts)[i].is_signer = updated_account_infos[i].is_signer;
+            //     (*accounts)[i].is_writable = updated_account_infos[i].is_writable;
+            //     (*accounts)[i].executable = updated_account_infos[i].executable;
+            //     (*accounts)[i].rent_epoch = updated_account_infos[i].rent_epoch;
+            // }
+            #[allow(clippy::transmute_ptr_to_ptr)]
+            #[allow(mutable_transmutes)]
+            let account_info_mut =
+                unsafe { std::mem::transmute::<&Pubkey, &mut Pubkey>(accounts[i].key) };
+            *account_info_mut = *updated_account_infos[i].key;
+            #[allow(clippy::transmute_ptr_to_ptr)]
+            #[allow(mutable_transmutes)]
+            let account_info_mut =
+                unsafe { std::mem::transmute::<&Pubkey, &mut Pubkey>(accounts[i].owner) };
+            *account_info_mut = *updated_account_infos[i].owner;
+            // #[allow(clippy::transmute_ptr_to_ptr)]
+            // #[allow(mutable_transmutes)]
+            // let account_info_mut =
+            //     unsafe { std::mem::transmute::<&u64, &mut u64>(&accounts[i].rent_epoch) };
+            // *account_info_mut = updated_account_infos[i].rent_epoch;
+            // #[allow(clippy::transmute_ptr_to_ptr)]
+            // #[allow(mutable_transmutes)]
+            // let account_info_mut =
+            //     unsafe { std::mem::transmute::<&bool, &mut bool>(&accounts[i].is_signer) };
+            // *account_info_mut = updated_account_infos[i].is_signer;
+            // #[allow(clippy::transmute_ptr_to_ptr)]
+            // #[allow(mutable_transmutes)]
+            // let account_info_mut =
+            //     unsafe { std::mem::transmute::<&bool, &mut bool>(&accounts[i].is_writable) };
+            // *account_info_mut = updated_account_infos[i].is_writable;
+            // #[allow(clippy::transmute_ptr_to_ptr)]
+            // #[allow(mutable_transmutes)]
+            // let account_info_mut =
+            //     unsafe { std::mem::transmute::<&bool, &mut bool>(&accounts[i].executable) };
+            // *account_info_mut = updated_account_infos[i].executable;
+        }
+        // for (dst, src) in accounts.iter().zip(updated_account_infos.iter()) {
+        //     println!("account {} updated: {}", dst.key, src.key);
+
+        //     **dst.lamports.borrow_mut() = src.lamports();
+        //     {
+        //         let src_data = src.data.borrow_mut();
+        //         let mut dst_data = dst.data.borrow_mut();
+        //         println!("NEW LEN: {}, OLD LEN: {}", src_data.len(), dst_data.len());
+
+        //         let _ = std::mem::replace(&mut dst_data, src_data);
+        //     }
+        // }
+
+        // let _ = std::fs::write("/tmp/old_accounts.txt", format!("{:#02x?}", accounts));
+        // let _ = std::fs::write("/tmp/old_data.txt", format!("{:#02x?}", data));
+        // let _ = std::fs::write("/tmp/new_data.txt", format!("{:#02x?}", _updated_data));
+        // panic!("STOP");
+        // // if data != updated_data {
+        // //     let mut_ref_data: &mut [u8] = unsafe { std::mem::transmute(data) };
+        // // }
+    }
+
+    if res == 0 {
+        Ok(())
+    } else {
+        Err(ProgramError::Custom(res as _))
+    }
 }
 
 #[derive(Debug, Default)]
@@ -88,9 +194,9 @@ impl Loader {
         self.libs
             .insert(*program_id, (program_name.to_string(), lib));
 
-        let fn_ptr = self.get_rust_entrypoint(program_id)?;
+        let entrypoint = self.get_entrypoint(program_id)?;
         let mut programs_map = PROGRAMS_MAP.lock().unwrap();
-        programs_map.insert(*program_id, AtomicPtr::new(fn_ptr as *mut _));
+        programs_map.insert(*program_id, AtomicPtr::new(entrypoint as *mut _));
         Ok(())
     }
 
@@ -99,27 +205,47 @@ impl Loader {
         program_id: &Pubkey,
         stubs_api: SyscallStubsApi,
     ) -> LiteCoverageError<()> {
-        let func: Symbol<ProgramSetSyscallStubsApi> = unsafe {
+        let res: Result<Symbol<ProgramSetSyscallStubsApi>, libloading::Error> = unsafe {
             self.libs
                 .get(program_id)
                 .ok_or("No such program_id".to_string())?
                 .1
-                .get(b"set_stubs")?
+                .get(b"set_stubs")
         };
-        unsafe { func(stubs_api) };
+        match res {
+            Ok(func) => unsafe { func(stubs_api) },
+            Err(e) => {
+                // REVISIT: The idea behind is this to try working with stubs
+                // and if not neccesary (as is the case with pinocchio as it
+                // seems for now) to continue
+                println!("Can't set stubs, error: {}! Go on", e);
+            }
+        }
         Ok(())
     }
 
-    fn get_rust_entrypoint(&self, program_id: &Pubkey) -> LiteCoverageError<Entrypoint> {
-        let func: Symbol<ProgramRustEntryPoint> = unsafe {
+    fn get_entrypoint(&self, program_id: &Pubkey) -> LiteCoverageError<ProgramEntrypoint> {
+        let entrypoint: Symbol<ProgramEntrypoint> = unsafe {
             self.libs
                 .get(program_id)
                 .ok_or("No such program_id".to_string())?
                 .1
-                .get(b"get_rust_entrypoint")?
+                .get(b"entrypoint")?
         };
-        let fn_ptr = unsafe { func() };
-        let rust_fn: Entrypoint = unsafe { std::mem::transmute(fn_ptr) };
-        Ok(rust_fn)
+        Ok(*entrypoint)
     }
 }
+
+// REVISIT: pinocchio's single syscall impl for now
+#[no_mangle]
+pub extern "C" fn sol_log_(msg: *const u8, len: u64) {
+    let message = unsafe { std::slice::from_raw_parts(msg, len as _) };
+    let m = str::from_utf8(&message).unwrap();
+    crate::stubs::SYSCALL_STUBS.read().unwrap().sol_log(&m);
+}
+
+#[no_mangle]
+pub extern "C" fn sol_log_pubkey(_pubkey: *const u8) {
+    println!("Unimplemented sol_log_pubkey syscall!");
+}
+// REVISIT
