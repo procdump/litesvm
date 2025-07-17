9a358d908bb741165ad40200a0c9c10a615c84ee
Boris Astardzhiev <boris.astardzhiev@gmail.com>
2025-07-17 13:05:42 +0300

    relocate stubs to the solana-program-stubs loader macro

--- crates/lite-coverage/src/stubs.rs
+++ crates/lite-coverage/src/stubs.rs
@@ -1,14 +1,15 @@
 use std::sync::{Arc, RwLock};
 
 use solana_program_error::ProgramResult;
-use solana_program_stubs::declare_sol_loader_stubs;
+use solana_program_stubs::declare_sol_loader_stubsv2;
 use solana_sysvar::program_stubs::SyscallStubs;
 
-use solana_instruction::AccountMeta;
 use solana_instruction::Instruction;
+use solana_program::instruction::AccountMeta;
 use solana_pubkey::Pubkey;
 use solana_sysvar::slot_history::AccountInfo;
-declare_sol_loader_stubs!();
+
+declare_sol_loader_stubsv2!();
 
 pub struct StubsManager;
 impl StubsManager {
